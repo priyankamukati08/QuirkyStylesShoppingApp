b6cdb261435b0d1dd0e1c7c072a1a95f69b8dc04
Priyanka Mukati <priyankamukati42@gmail.com>
2024-03-11 23:52:26 -0700

    swagger working

--- ShoppingApp-Frontend/src/store/reducers/rootReducer.js
+++ ShoppingApp-Frontend/src/store/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import productReducer from "./productReducer";
 import productByIDReducer from "./productByIDReducer";
 import userCartReducer from "./userCartReducer";
 import addProductToUserCartReducer from "./addProductUserCartReducer";
+import addUserInfoReducer from "./addUserInfoReducer";
 
 const rootReducer = combineReducers({
   products: productReducer,
   productbyid: productByIDReducer,
   userCart: userCartReducer,
   addProductToUserCart: addProductToUserCartReducer,
+  addUserInfo: addUserInfoReducer,
 });
 
 export default rootReducer;
